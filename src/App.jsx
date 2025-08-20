cd4aeb551d68ea1a9b15019cf7b95ee46543f419
dilayuluerguven <uluerguven245@gmail.com>
2025-08-20 15:36:32 +0300

    add use-react-screenshot for capturing component screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import {
   CheckCircleFilled,
   HeartOutlined,
@@ -9,17 +9,35 @@ import {
 } from "@ant-design/icons";
 import "./App.css";
 import "./css/style.scss";
+import { AvatarLoader } from "./loaders";
+import { useScreenshot } from 'use-react-screenshot'
+
 
 const tweetFormat = (tweet) => {
   tweet = tweet
-    .replace(/@([\w]+)/g, '<span>@$1</span>')
-    .replace(/#([\wşçöğüı]+)/gi, '<span>#$1</span>')
-    .replace(/(https?:\/\/[\w\.\/]+)/g, '<span>$1</span>');
+    .replace(/@([\w]+)/g, "<span>@$1</span>")
+    .replace(/#([\wşçöğüı]+)/gi, "<span>#$1</span>")
+    .replace(/(https?:\/\/[\w\.\/]+)/g, "<span>$1</span>");
   return tweet;
 };
+const formatNumber = (number) => {
+  if (!number) {
+    number = 0;
+  }
 
+  if (number < 1000) {
+    return number;
+  }
+  number /= 1000;
+  number = String(number).split(".");
+
+  return (
+    number[0] + (number[1] > 100 ? "," + number[1].slice(0, 1) + "B" : "B")
+  );
+};
 
 function App() {
+  const tweetRef = useRef(null)
   const [name, setName] = useState();
   const [username, setUsername] = useState();
   const [isVerified, setIsVerified] = useState(false);
@@ -28,47 +46,98 @@ function App() {
   const [retweets, setRetweets] = useState(0);
   const [quoteTweets, setQuoteTweets] = useState(0);
   const [likes, setLikes] = useState(0);
+  const [image, takeScreenshot] = useScreenshot()
+  const getImage = () => takeScreenshot(tweetRef.current)
+
+useEffect(() => {
+  
+  console.log(image)
+}, [image])
+
+
+
+  const avatarHandle = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", function () {
+      setAvatar(this.result);
+    });
+    reader.readAsDataURL(file);
+  };
   return (
     <>
       <div className="tweet-settings">
         <h3>Tweet Ayarları</h3>
         <ul>
           <li>
+            <label>Ad Soyad</label>
             <input
               className="input"
               type="text"
-              placeholder="Ad Soyad "
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </li>
           <li>
+            <label>Kullanıcı Adı</label>
             <input
               className="input"
               type="text"
-              placeholder="Kullanıcı Adı "
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </li>
           <li>
+            <label>Tweet</label>
             <textarea
               maxLength="290"
               class="textarea"
-              placeholder="Tweet"
               value={tweet}
               onChange={(e) => setTweet(e.target.value)}
             />
           </li>
+          <li>
+            <label>Avatar</label>
+            <input className="input" type="file" onChange={avatarHandle} />
+          </li>
+          <li>
+            <label>Retweet</label>
+            <input
+              className="input"
+              type="number"
+              value={retweets}
+              onChange={(e) => setRetweets(e.target.value)}
+            />
+          </li>
+          <li>
+            <label>Alıntı Tweet</label>
+            <input
+              className="input"
+              type="number"
+              value={quoteTweets}
+              onChange={(e) => setQuoteTweets(e.target.value)}
+            />
+          </li>
+          <li>
+            <label>Beğeni</label>
+            <input
+              className="input"
+              type="number"
+              value={likes}
+              onChange={(e) => setLikes(e.target.value)}
+            />
+          </li>
+          <button onClick={getImage}> Oluştur</button>
+            <div className="download-url">
+             {image && (<a href={image} download="tweet.png" >Tweeti İndir</a>)}
+
+            </div>
         </ul>
       </div>
       <div className="tweet-container">
-        <div className="tweet">
+        <div className="tweet" ref={tweetRef}>
           <div className="tweet-author">
-            <img
-              src="https://cdn.myikas.com/images/e0f17726-ad7c-4b1a-a568-7bc56544bad8/7d7730b6-6cba-44b8-a6d6-44aa45b9521e/image_1080.jpg"
-              alt=""
-            />
+            {(avatar && <img src={avatar} />) || <AvatarLoader />}
             <div>
               <div className="name">
                 {name || "Ad Soyad"}
@@ -94,13 +163,13 @@ function App() {
           </div>
           <div className="tweet-stats">
             <span>
-              <b>{retweets}</b> Retweet
+              <b>{formatNumber(retweets)}</b> Retweet
             </span>
             <span>
-              <b>{quoteTweets} </b> Alıntı Tweetler
+              <b>{formatNumber(quoteTweets)} </b> Alıntı Tweetler
             </span>
             <span>
-              <b>{likes}</b> Beğeni
+              <b>{formatNumber(likes)}</b> Beğeni
             </span>
           </div>
           <div className="tweet-actions">
