fead363ee4f24a448d26399e473a7ce85d7f3170
dilayuluerguven <uluerguven245@gmail.com>
2025-08-20 14:00:54 +0300

    Updated homepage styles and fixed designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useState } from "react";
+import {
+  CheckCircleFilled,
+  HeartOutlined,
+  MessageOutlined,
+  RetweetOutlined,
+  ShareAltOutlined,
+  VerifiedOutlined,
+} from "@ant-design/icons";
+import "./App.css";
+import "./css/style.scss";
 function App() {
-
-
+  const [name, setName] = useState();
+  const [username, setUsername] = useState();
+  const [isVerified, setIsVerified] = useState(false);
+  const [tweet, setTweet] = useState();
+  const [avatar, setAvatar] = useState();
+  const [retweets, setRetweets] = useState(0);
+  const [quoteTweets, setQuoteTweets] = useState(0);
+  const [likes, setLikes] = useState(0);
   return (
     <>
-      hello
+      <div className="tweet-settings">
+        <h3>Tweet Ayarları</h3>
+      </div>
+      <div className="tweet-container">
+        <div className="tweet">
+          <div className="tweet-author">
+            <img
+              src="https://cdn.myikas.com/images/e0f17726-ad7c-4b1a-a568-7bc56544bad8/7d7730b6-6cba-44b8-a6d6-44aa45b9521e/image_1080.jpg"
+              alt=""
+            />
+            <div>
+              <div className="name">
+                {name || "Ad Soyad"}
+                {isVerified && (
+                  <CheckCircleFilled
+                    style={{
+                      alignItems: "center",
+                      borderRadius: "50%",
+                    }}
+                  />
+                )}
+              </div>
+              <div className="username">@{username || "kullaniciadi"}</div>
+            </div>
+          </div>
+          <div className="tweet-content">
+            <p>{tweet || 'Bu alana tweet gelecek'}</p>
+          </div>
+          <div className="tweet-stats">
+            <span>
+              <b>24 </b> Retweet
+            </span>
+            <span>
+              <b>53 </b> Alıntı Tweetler
+            </span>
+            <span>
+              <b>782 </b> Beğeni
+            </span>
+          </div>
+          <div className="tweet-actions">
+            <span className="tweet-icons">
+              <MessageOutlined />
+            </span>
+            <span className="tweet-icons">
+              <RetweetOutlined />
+            </span>
+            <span className="tweet-icons">
+              <HeartOutlined />
+            </span>
+            <span className="tweet-icons">
+              <ShareAltOutlined />
+            </span>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
